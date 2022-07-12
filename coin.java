c57d7dad14d8826f0532f1d684ab31ad55949179
s1290067 <s1290067@u-aizu.ac.jp>
2022-07-12 16:48:46 +0900

    first commit

---
+++ coin.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class coin{
+    public static void main(String[] args) {
+	int[] a = {0,1};
+	int j = 0, m = 0;
+	Random r = new Random();
+	System.out.println("Tossing a coin...");
+	for(int i = 1; i < 4; i++) {
+	    int b = a[r.nextInt(2)];
+	    System.out.print("Round "+ i+":");
+	    if(b == 0) {
+		System.out.println(" Heads");
+		j++;
+	    }
+	    else {
+		System.out.println(" Tails");
+		m++;
+	    }
+	}
+	System.out.println("Heads: "+j+", Tails: "+m);
+    }
+}
